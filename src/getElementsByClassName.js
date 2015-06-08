9aa18f4212ea3b8e4fb9c3661db4f99712f3f2f0
MKS-Student <mks.student@hackreactor.com>
2015-06-08 15:53:25 -0700

    recursion sprint passed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className, node, results){
   // your code here
+  var node = node || document.body;
+  var results = results || [];
+  if (node.classList.contains(className)) {
+    results.push(node);
+  }
+  for (var i = 0, children = node.children, childrenLen = children.length; i < childrenLen; i ++) {
+    getElementsByClassName(className, children[i], results);
+  }
+
+  return results;
+    // base case: if node has className
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  if(Array.isArray(obj)) {
+    var res = [];
+    for (var i = 0; i < obj.length; i++){
+      res.push(stringifyJSON(obj[i]));
+    }
+    return "[" + res.join(',') + "]";
+
+  } 
+  if(typeof obj === 'object' && obj !== null){
+    var res = [];
+    for(i in obj){
+      if(typeof obj[i] === 'function' || typeof obj[i] === 'undefined') {
+        continue;
+
+      }
+      res.push( stringifyJSON(i) + ":" + stringifyJSON(obj[i]) );
+    }
+    return "{"+ res.join(",")+ "}";
+  }
+  return "" + obj;
   // your code goes here
 };
